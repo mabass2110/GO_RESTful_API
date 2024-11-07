3c1cb350404f54bf4e437935563b4639bb89c989
Helder Mabasso <hfmabasso@gmail.com>
2024-11-08 01:28:16 +0200

    Fixing a little bug.

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ type album struct {
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
-3
+
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
