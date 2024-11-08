b3d2863f4f6e8d395c730b95c97fca7d54c94f2c
Helder Mabasso <hfmabasso@gmail.com>
2024-11-08 19:07:03 +0200

    POST request to store a newAlbum in Albums

--- main.go
+++ main.go
@@ -25,10 +25,25 @@ func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
 
+func postAlbums(c *gin.Context) {
+
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
 func main() {
 	router := gin.Default()
-
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
